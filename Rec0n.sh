8a5dcfa8d9e3895ed40a333db0785d01cdd763fa
Rajiv Sharma <cyberoxian@gmail.com>
2023-11-13 11:58:45 +0530

    update of source file

--- Rec0n.sh
+++ Rec0n.sh
@@ -77,6 +77,7 @@ sort -u $url/recon/a.txt > $url/recon/alive.txt
 echo -e "\e[32m[+]\e[0m Total Number of Alive domain's Discovered \e[31m"$(cat $url/recon/alive.txt | wc -l)"\e[0m from $url"
 rm $url/recon/a.txt
 
+cat $url/recon/alive.txt | httpx-toolkit -sc -td -cname -probe -ip
 
 cat $url/recon/alive.txt | httpx-toolkit -sc >> $url/recon/status.txt
 cat $url/recon/status.txt |grep "200" >> $url/recon/200.txt
